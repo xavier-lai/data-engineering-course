5ba26998a6d1577b43b30c97ce952da7d7824684
Xavier Lai <xavier.l@cabaia.fr>
2024-11-16 20:27:20 +0100

    import route

--- src/event_api/app/routes/__init__.py
+++ src/event_api/app/routes/__init__.py
@@ -1,4 +1,9 @@
 from .get__all_events import router as get_all_events_router
+from .get__events_by_weekday import router as get_events_by_weekday_router
 from .get__search_events import router as get_search_events_router
 
-all_routes = [get_all_events_router, get_search_events_router]
+all_routes = [
+    get_all_events_router,
+    get_search_events_router,
+    get_events_by_weekday_router,
+]
